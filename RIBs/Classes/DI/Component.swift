da9192349b1ac90efdb6069807ef1dbabf865f6b
Alex Bush <alex.v.bush@gmail.com>
2025-10-11 16:28:40 -0500

    Mark builder and component as nonisolated

--- RIBs/Classes/DI/Component.swift
+++ RIBs/Classes/DI/Component.swift
@@ -23,7 +23,7 @@ import Foundation
 ///
 /// A component subclass implementation should conform to child 'Dependency' protocols, defined by all of its immediate
 /// children.
-open class Component<DependencyType>: Dependency {
+nonisolated open class Component<DependencyType>: Dependency {
 
     /// The dependency of this `Component`.
     public let dependency: DependencyType
