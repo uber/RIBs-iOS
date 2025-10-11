78594f6078e1bfe756a0a4950fea0752ae3041bd
Alex Bush <alex.v.bush@gmail.com>
2025-10-11 16:28:40 -0500

    Mark builder and component as nonisolated

--- RIBs/Classes/Builder.swift
+++ RIBs/Classes/Builder.swift
@@ -20,7 +20,7 @@ import Foundation
 nonisolated public protocol Buildable: AnyObject {}
 
 /// Utility that instantiates a RIB and sets up its internal wirings.
-open class Builder<DependencyType>: Buildable {
+nonisolated open class Builder<DependencyType>: Buildable {
 
     /// The dependency used for this builder to build the RIB.
     public let dependency: DependencyType
